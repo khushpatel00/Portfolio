2fd336c3fc08e37beeaf09f6c65f4256130c4fe6
khushpatel00 <patelxkhush@gmail.com>
2026-01-22 22:34:28 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Landing from './Components/Landing'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Achievements from './Components/Achievements'
+import Skills from './Components/Skills'
+import Education from './Components/Education'
+import Contact from './Components/Contact'
+
+function App() {
+  return (
+    <div>
+      <Landing />
+      <About />
+      <Projects />
+      <Achievements />
+      <Skills />
+      <Education />
+      <Contact />
+
+    </div>
+  )
+}
+
+export default App
