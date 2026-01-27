ad8e80d9065276896647af674cae90bdbeb18e13
khushpatel00 <patelxkhush@gmail.com>
2026-01-27 16:52:55 +0530

    added: lab, with projects

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react'
 import { gsap } from 'gsap';
+import {Link} from 'react-router-dom'
 
 function Menu() {
     // useGSAP(() => {
@@ -20,33 +21,25 @@ function Menu() {
     //     })
     //     tl.set('.stairsParent',{ display: 'none' })
 
-    //     // openMenu = () => {
-    //     //     tl.set('.stairsParent',{ display: 'flex' })
-    //     //     tl.from(stairs, {
-    //     //     y: '-100%',
-    //     //     stagger: 0.1,
-    //     //     duration: 1,
-    //     //     ease: 'power2.out'
-    //     // })}
+    //     openMenu = () => {
+    //         tl.set('.stairsParent',{ display: 'flex' })
+    //         tl.from(stairs, {
+    //         y: '-100%',
+    //         stagger: 0.1,
+    //         duration: 1,
+    //         ease: 'power2.out'
+    //     })}
 
     // });
 
     const stairRef = useRef([]);
     const iconRef = useRef(null);
     const tl = gsap.timeline();
-    // useEffect(()=>{
-    //     console.log(stairRef.current)
-    //     tl.to(stairRef.current, {
-    //         y: 100,
-    //         stagger: 0.1,
-    //         duration: 1,
-    //         ease: 'power2.out'
-    //     })
-    //     tl.set('.stairsParent',{ display: 'flex' });    
-    // })
+    
+
 
     let isClosed = true;
-    const handleClick = ()=>{
+    const handleClick = () => {
         console.log('clicked', stairRef.current);
         if(isClosed){
             tl.set('.stairsParent',{ display: 'flex' })
@@ -59,6 +52,15 @@ function Menu() {
             // iconRef.current.src='close.svg';
             iconRef.current.children[0].className = 'duration-300 m-0 p-0 translate-y-1/2 w-8 h-px rotate-45 bg-white'
             iconRef.current.children[1].className = 'duration-300 m-0 p-0 -translate-y-1/2 w-8 h-px -rotate-45 bg-white'
+            gsap.fromTo('.content', {
+                y: '-20',
+                delay: 0.5,
+                opacity: 0
+            }, {
+                y: 0,
+                delay: 0.5,
+                opacity: 1
+            })
             isClosed = false;
         } else{
             tl.set('.stairsParent',{ display: 'flex' })
@@ -77,24 +79,34 @@ function Menu() {
             })
             isClosed = true
             tl.set('.stairsParent', {display: 'none'});
+            gsap.to('.content', {
+                y: 20,
+                opacity: 0,
+            })
+            
         }
     };
-
-
+    useEffect(()=>{
+        if(window.location.href.split('/').pop() == 'lab'){
+            isClosed = false;
+            handleClick();
+        }
+    })
     return (
         <>
-            <div className='fixed duration-300 z-10 flex flex-col items-center justify-center top-0 right-0 bg-black w-1/8 h-10 px-20 cursor-pointer' id="menuButton" ref={iconRef} onClick={handleClick}>
-                
+            <div className='fixed duration-300 z-20 flex flex-col items-center justify-center top-0 right-0 bg-black w-1/8 h-10 px-20 cursor-pointer' id="menuButton" ref={iconRef} onClick={handleClick}>                
                     <div className='duration-300 w-8 h-px my-1 bg-white'></div>
                     <div className='duration-300 w-8 h-px my-1 bg-white'></div>
-            
             </div>
-
-            <div className='absolute top-0 left-0'>
+            <div className='absolute top-0 left-0 z-15'>
                 <div className="stairsParent relative h-screen w-screen hidden bg-transparent">
                     {[1, 2, 3, 4, 5].map((val, index) => (
                         <div key={index} ref={(el) => (stairRef.current[index] = el)} className="stair w-1/5 h-full bg-black"></div>
                     ))}
+                    <div className='flex flex-col z-20 content absolute top-1/2 left-1/2 -translate-1/2'>
+                        <Link className='text-4xl text-bold poppins'>Home</Link>
+                        <Link to='/lab' className='text-4xl text-bold poppins'>Lab</Link>
+                    </div>
                 </div>
             </div>
         </>
--- src/Pages/Lab.jsx
+++ src/Pages/Lab.jsx
@@ -1,9 +1,87 @@
 import React from 'react'
+import InfiniteMenu from '../Components/InfiniteMenu'
+import Menu from '../Components/Menu'
 
-function Projects() {
+const items = [
+  {
+    image: 'https://picsum.photos/300/300?grayscale',
+    link: 'https://google.com/',
+    title: 'Ochi',
+    description: ' FramerMotion'
+  },
+  {
+    image: 'https://picsum.photos/400/400?grayscale',
+    link: 'https://google.com/',
+    title: 'DBSeeder',
+    description: 'we made this in a hackathon, btw'
+  },
+  {
+    image: 'https://picsum.photos/500/500?grayscale',
+    link: 'https://google.com/',
+    title: 'session0',
+    description: 'i didnt completed the whole project 🙂'
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'FakeStore',
+    description: 'i just implemented the use of This API (not a project btw)'
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'Driveman',
+    description: "i'm a gamedev too, btw"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'WeatherNow',
+    description: "a mini-project made with WeatherAPI"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'FINDr',
+    description: "a mini-project movie-finder project (i didnt finished this too)"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'CoffeeMarket',
+    description: "a CoffeeMarket UI (no-js)"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'Dekoro',
+    description: "a Furniture-page UI (no-js)"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'A2Z Furnishing',
+    description: "Live-project (no-js)"
+  },
+  {
+    image: 'https://picsum.photos/600/600?grayscale',
+    link: 'https://google.com/',
+    title: 'Nestify',
+    description: "a Ecommerce-page UI (no-js)"
+  },
+];
+
+function Lab() {
   return (
-    <div>Projects</div>
+    <>
+      <Menu />
+      <div style={{ height: '100vh', position: 'relative' }}>
+        <InfiniteMenu items={items}
+          scale={1}
+        />
+      </div>
+    </>
   )
 }
 
-export default Projects
+export default Lab;
