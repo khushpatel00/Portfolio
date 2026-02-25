25426423f945381415f8464cd65ffa974dac118c
khushpatel00 <patelxkhush@gmail.com>
2026-02-25 21:58:17 +0530

    added: contact, and resume exhanced: lenis scrolling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,9 +4,29 @@ function Contact() {
   return (
     <div id='contact' className={'md:overflow-hidden sm:overflow-y-hidden md:overflow-y-hidden lg:overflow-x-hidden md:overflow-x-hidden sm:overflow-x-hidden overflow-x-hidden'}>
       <h2 className={'text-7xl w-2/3 mx-auto px-5 mt-20 bricolage font-bold'}>Contact Me</h2>
-      <div className='flex w-2/3 mx-auto flex-row flex-wrap gap-0.5'>
-        <h4 className={'text-2xl ps-10'}>React me out on: </h4>
-        
+      <div className='flex w-2/3 ps-10 mt-2.5 mx-auto flex-col flex-wrap gap-0.5'>
+        {/* <h4 className={'text-2xl '}>React me out on: </h4> */}
+        <div className='text-2xl flex flex-row flex-wrap items-end ps-5'>
+
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>Resume</h5>: <a href='/khushpatel-resume.pdf' download target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }} className='jet ps-3'>Download</a>
+          </div>
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>Github</h5>: <a href='https://github.com/khushpatel00' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }} className='jet ps-3'>khushpatel00</a>
+          </div>
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>LinkedIn</h5>: <a href='https://linkedin.com/in/khushpatel00' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }} className='jet ps-3'>khushpatel00</a>
+          </div>
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>Mail</h5>: <a href='mailto:patelxkhush@gmail.com' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }} className='jet ps-3'>patelxkhush@gmail.com</a>
+          </div>
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>Instagram</h5>: <a href='https://instagram.com/iduunnowhy' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }} className='jet ps-3'>iduunnowhy</a>
+          </div>
+          <div className='flex flex-row items-end basis-1/2'>
+            <h5 className='text-3xl font-semibold bricolage'>Discord</h5>: <a target='_blank' className='jet ps-3'>@realnigg_.</a>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,22 +16,24 @@ function Home() {
     return (
         <>
             <Menu />
-            <div data-scroll data-scroll-speed="-0.2">
+            <div data-scroll data-scroll-speed="-0.2" data-scroll-section>
                 <Landing />
             </div>
             <div data-scroll data-scroll-speed="0.1" className='bg-white'>
                 <About />
                 <Skills />
             </div>
-            <div data-scroll data-scroll-speed="0">
+            <div data-scroll data-scroll-speed="-0.2" className='mt-28'>
                 <Projects />
                 <Playground />
                 <Achievements />
                 {/* <Education /> */}
             </div>
-            <Contact />
+            <div data-scroll data-scroll-speed='0.2' className='my-20'>
+                <Contact />
+            </div>
 
-            <div className={'mx-auto bg-amber-100 rounded-t-2xl w-4/5 py-5 px-1 mt-20 text-center text-2xl'} >This Portfolio is Under Development, Feel Free to raise an issue on <a href='https://github.com/khushpatel00/portfolio' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }}>Github</a>, if you find any bug, or feel something off</div>
+            <div data-scroll data-scroll-speed='0.1' className={'mx-auto bg-amber-100 rounded-t-2xl w-4/5 py-5 px-1 mt-20 text-center text-2xl'} >This Portfolio is Under Development, Feel Free to raise an issue on <a href='https://github.com/khushpatel00/portfolio' target='_blank' style={{ textDecoration: 'underline wavy 1px #3f3f46' }}>Github</a>, if you find any bug, or feel something off</div>
         </>
     )
 }
